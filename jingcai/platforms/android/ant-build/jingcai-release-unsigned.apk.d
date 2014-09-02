f271ae918e5a9ef5367580ccc131222617e40db6
幻悟 <huanwu.yd@taobao.com>
2014-09-02 13:55:33 +0800

    5
    5

---
+++ jingcai/platforms/android/ant-build/jingcai-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+/Users/dong/m/jingcai/platforms/android/ant-build/jingcai-release-unsigned.apk : \
+/Users/dong/m/jingcai/platforms/android/ant-build/jingcai.ap_ \
+/Users/dong/m/jingcai/platforms/android/ant-build/classes.dex \
+/Users/dong/m/jingcai/platforms/android/CordovaLib/ant-build/classes.jar \
